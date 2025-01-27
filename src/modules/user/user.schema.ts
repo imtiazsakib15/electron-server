5cf01a69a0f1c716bcd5beba5d3ae5e0430e2be1
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2025-01-27 13:50:47 +0600

    user schema created

---
+++ src/modules/user/user.schema.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod';
+
+export const UserRole = z.enum(['USER', 'ADMIN']);
+
+export const UserSchema = z.object({
+  _id: z.string().optional(),
+  name: z
+    .string()
+    .trim()
+    .min(1, 'Name is required')
+    .max(100, 'Name must be less than 100 characters'),
+  email: z.string().email('Invalid email format'),
+  password: z
+    .string()
+    .min(6, 'Password must be at least 6 characters')
+    .max(32, 'Password must be less than 32 characters')
+    .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
+    .regex(/[0-9]/, 'Password must contain at least one digit'),
+  role: UserRole.default('USER'),
+  createdAt: z.string().datetime({ offset: true }).optional(),
+  updatedAt: z.string().datetime({ offset: true }).optional(),
+  isDeleted: z.boolean().default(false),
+});
+
+export const CreateUserSchema = UserSchema.omit({
+  _id: true,
+  createdAt: true,
+  updatedAt: true,
+});
+export const UpdateUserSchema = UserSchema.partial().omit({
+  _id: true,
+  createdAt: true,
+  updatedAt: true,
+});
